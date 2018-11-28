8bc154b7f3fb1d7543581362bb56591c236a560f
QiangXie <happyxieqiang@qq.com>
2018-11-28 11:09:00 +0800

    modify ~/ to abs home path

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ import subprocess
 import shutil
 import getpass
 
+#get user name
+username = getpass.getuser()
+
 #htop
 if not os.path.exists("/usr/bin/htop"):
     print("htop hasn't been installed, please install htop first.")
@@ -32,15 +35,14 @@ print("Config zsh...")
 if not os.path.exists("/usr/bin/zsh"):
     print("zsh hasn't been installed, please install zsh first.")
     exit(1)
-if not os.path.exists("~/.zshrc"):
-    subprocess.call("cp ./.zshrc ~/", shell = True)
+if not os.path.exists("/home/{}/.zshrc".format(username)):
+    subprocess.call("cp ./.zshrc /home/{}/".format(username), shell = True)
 
-username = getpass.getuser()
 shell_string = "cat /etc/passwd | grep \"^" + username +"\\>\" | awk -v FS=: '{print $7}'"
 p = subprocess.Popen(shell_string, shell=True, stdout=subprocess.PIPE)
 shell_type = p.stdout.readlines()[0].decode('utf-8').strip()
 if not shell_type == "/bin/zsh":
-    print("Change default shell to zsh, please input your password:")
+    print("Default shell is {}, change default shell to zsh, please input your password:".format(shell_type))
     subprocess.call("chsh -s /bin/zsh", shell = True)
 print("Config zsh done.")
 
@@ -49,18 +51,20 @@ print("Config tmux...")
 if not os.path.exists("/usr/bin/tmux"):
     print("tmux hasn't been installed, please install tmux first.")
     exit(1)
-if not os.path.exists("~/.tmux.conf"):
-    subprocess.call("cp ./.tmux.conf ~/", shell = True)
+if not os.path.exists("/home/{}/tmux.conf".format(username)):
+    subprocess.call("cp ./.tmux.conf /home/{}/".format(username), shell = True)
 print("Config tmux done.")
 
 #config vim 
 print("Config vim...")
-subprocess.call("cp ./.vimrc ~/", shell = True)
-if not os.path.exists("~/.vim/bundle/Vundle.vim"):
-    subprocess.call("git clone https://github.com/VundleVim/Vundle.vim.git  ~/.vim/bundle/Vundle.vim", shell = True)
+subprocess.call("cp ./.vimrc /home/{}/".format(username), shell = True)
+if not os.path.exists("/home/{}/.vim/bundle/Vundle.vim".format(username)):
+    subprocess.call("git clone https://github.com/VundleVim/Vundle.vim.git  /home/{}/.vim/bundle/Vundle.vim".format(username),
+            shell = True)
 else:
-    shutil.rmtree("~/.vim/bundle/Vundle.vim")
-    subprocess.call("git clone https://github.com/VundleVim/Vundle.vim.git  ~/.vim/bundle/Vundle.vim", shell = True)
+    shutil.rmtree("/home/{}/.vim/bundle/Vundle.vim".format(username))
+    subprocess.call("git clone https://github.com/VundleVim/Vundle.vim.git  /home/{}/.vim/bundle/Vundle.vim".format(username), 
+            shell = True)
 subprocess.call("vim +BundleInstall +qall", shell = True)
-subprocess.call("python ~/.vim/bundle/YouCompleteMe/install.py", shell = True)
+subprocess.call("python /home/{}/.vim/bundle/YouCompleteMe/install.py".format(username), shell = True)
 print("Config vim done." )
